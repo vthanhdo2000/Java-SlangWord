80f9a2db777e5cfa112f49c863212b659f033a52
HarryThanhDo <harry.thanh@namtech.sg>
2022-07-08 09:49:14 +0700

    add function read/save data

--- src/slangwordapp/Service.java
+++ src/slangwordapp/Service.java
@@ -5,10 +5,159 @@
  */
 package slangwordapp;
 
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+
 /**
  *
  * @author acer
  */
 public class Service {
-    
+
+    private TreeMap<String, List<String>> map = new TreeMap<>();
+    private static Service obj = new Service();
+    private int sizeMap;
+    private String FILE_SLANGWORD = "slang.txt";
+    private String FILE_ORIGINAL_SLANGWORD = "slang-root.txt";
+    private String FILE_HISTORY = "history.txt";
+
+    Service() {
+        try {
+            String current = new java.io.File(".").getCanonicalPath();
+            System.out.println("Current dir:" + current);
+            FILE_SLANGWORD = current + "\\" + FILE_SLANGWORD;
+            FILE_ORIGINAL_SLANGWORD = current + "\\" + FILE_ORIGINAL_SLANGWORD;
+            FILE_HISTORY = current + "\\" + FILE_HISTORY;
+            readFile(FILE_SLANGWORD);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static Service getInstance() {
+        if (obj == null) {
+            synchronized (Service.class) {
+                if (obj == null) {
+                    obj = new Service();// instance will be created at request time
+                }
+            }
+        }
+        return obj;
+    }
+
+    void readFile(String file) throws Exception {
+        map.clear();
+        String slag = null;
+        Scanner scanner = new Scanner(new File(file));
+        scanner.useDelimiter("`");
+        scanner.next();
+        String temp = scanner.next();
+        String[] part = temp.split("\n");
+        int i = 0;
+        int flag = 0;
+        sizeMap = 0;
+        while (scanner.hasNext()) {
+            List<String> meaning = new ArrayList<String>();
+            slag = part[1].trim();
+            temp = scanner.next();
+            part = temp.split("\n");
+            if (map.containsKey(slag)) {
+                meaning = map.get(slag);
+            }
+            if (part[0].contains("|")) {
+                //System.out.println(part[0]);
+                String[] d = (part[0]).split("\\|");
+                //for (int ii = 0; ii < d.length; ii++) {
+                    //System.out.println(d[ii]);
+                //}
+                Collections.addAll(meaning, d);
+                sizeMap += d.length - 1;
+            } else {
+                meaning.add(part[0]);
+            }
+            // map.put(slag.trim(), meaning);
+            map.put(slag, meaning);
+            i++;
+            sizeMap++;
+        }
+        scanner.close();
+    }
+
+    void saveFile(String file) {
+        try {
+            PrintWriter printWriter = new PrintWriter(new File(file));
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.append("Slag`Meaning\n");
+            String s[][] = new String[map.size()][3];
+            Set<String> keySet = map.keySet();
+            Object[] keyArray = keySet.toArray();
+            for (int i = 0; i < map.size(); i++) {
+                Integer in = i + 1;
+                s[i][0] = in.toString();
+                s[i][1] = (String) keyArray[i];
+                List<String> meaning = map.get(keyArray[i]);
+                stringBuilder.append(s[i][1] + "`" + meaning.get(0));
+                for (int j = 1; j < meaning.size(); j++) {
+                    stringBuilder.append("|" + meaning.get(j));
+                }
+                stringBuilder.append("\n");
+            }
+            // System.out.println(stringBuilder.toString());
+            printWriter.write(stringBuilder.toString());
+            printWriter.close();
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e);
+        }
+    }
+
+    public String[][] getData() {
+        String s[][] = new String[sizeMap][3];
+        Set<String> slagListSet = map.keySet();
+        Object[] slagList = slagListSet.toArray();
+        int index = 0;
+        for (int i = 0; i < sizeMap; i++) {
+            s[i][0] = String.valueOf(i);
+            s[i][1] = (String) slagList[index];
+            List<String> meaning = map.get(slagList[index]);
+            s[i][2] = meaning.get(0);
+            System.out.println(s[i][0] + "\t" + s[i][1] + "\t" + s[i][2]);
+            for (int j = 1; j < meaning.size(); j++) {
+                if (i < sizeMap) {
+                    i++;
+                }
+                s[i][0] = String.valueOf(i);
+                s[i][1] = (String) slagList[index];
+                s[i][2] = meaning.get(j);
+                System.out.println(s[i][0] + "\t" + s[i][1] + "\t" + s[i][2]);
+            }
+            index++;
+        }
+        return s;
+    }
+
+    public String[][] getMeaning(String key) {
+        List<String> listMeaning = map.get(key);
+        if (listMeaning == null) {
+            return null;
+        }
+        int size = listMeaning.size();
+        String s[][] = new String[size][3];
+        for (int i = 0; i < size; i++) {
+            s[i][0] = String.valueOf(i);
+            s[i][1] = key;
+            s[i][2] = listMeaning.get(i);
+        }
+        return s;
+    }
+
 }
--- src/slangwordapp/SlangWordApp.java
+++ src/slangwordapp/SlangWordApp.java
@@ -17,83 +17,118 @@ public class SlangWordApp {
     /**
      * @param args the command line arguments
      */
-    public static void printMenu(String[] options){
-        for (String option : options){
+    public static void printMenu(String[] options) {
+        for (String option : options) {
             System.out.println(option);
         }
         System.out.print("Choose your option : ");
     }
-    
+
     public static void main(String[] args) {
+
+        Service service = Service.getInstance();
+
         // TODO code application logic here
         String[] options = {"1- Search by slang word",
-                            "2- Search by definition",
-                            "3- Display history, list of searched slang words",
-                            "4- Add a new slang words",
-                            "5- Edit a slang words",
-                            "6- Delete a slang words. Confirm before deleting",
-                            "7- Reset the original slang words list",
-                            "8- Random 1 slang word (On this day slang word)",
-                            "9- Quiz game, the program displays 1 random slang word, with 4 answers for user selects.",
-                            "10- Quiz game, the program displays 1 definition, with 4 slang words the answer for the user to choose",
-                            "11- Exit",
-        };
+            "2- Search by definition",
+            "3- Display history, list of searched slang words",
+            "4- Add a new slang words",
+            "5- Edit a slang words",
+            "6- Delete a slang words. Confirm before deleting",
+            "7- Reset the original slang words list",
+            "8- Random 1 slang word (On this day slang word)",
+            "9- Quiz game, the program displays 1 random slang word, with 4 answers for user selects.",
+            "10- Quiz game, the program displays 1 definition, with 4 slang words the answer for the user to choose",
+            "11- Exit",};
         Scanner scanner = new Scanner(System.in);
         int option = 1;
-        while (option!=11){
+        while (option != 11) {
             printMenu(options);
             try {
                 option = scanner.nextInt();
-                switch (option){
-                    case 1: option1(); break;
-                    case 2: option2(); break;
-                    case 3: option3(); break;
-                    case 4: option4(); break;
-                    case 5: option5(); break;
-                    case 6: option6(); break;
-                    case 7: option7(); break;
-                    case 8: option8(); break;
-                    case 9: option9(); break;
-                    case 10: option10(); break;
-                    case 11: exit(0);
+                switch (option) {
+                    case 1:
+                        option1();
+                        break;
+                    case 2:
+                        option2();
+                        break;
+                    case 3:
+                        option3();
+                        break;
+                    case 4:
+                        option4();
+                        break;
+                    case 5:
+                        option5();
+                        break;
+                    case 6:
+                        option6();
+                        break;
+                    case 7:
+                        option7();
+                        break;
+                    case 8:
+                        option8();
+                        break;
+                    case 9:
+                        option9();
+                        break;
+                    case 10:
+                        option10();
+                        break;
+                    case 11:
+                        exit(0);
                 }
-            }
-            catch (Exception ex){
+            } catch (Exception ex) {
                 System.out.println("Please enter an integer value between 1 and " + options.length);
                 scanner.next();
             }
         }
     }
-    
+
     private static void option1() {
-        System.out.println("Thanks for choosing option 1");
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username");
+
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println("Username is: " + userName);
     }
+
     private static void option2() {
         System.out.println("Thanks for choosing option 2");
     }
+
     private static void option3() {
         System.out.println("Thanks for choosing option 3");
     }
+
     private static void option4() {
         System.out.println("Thanks for choosing option 4");
     }
+
     private static void option5() {
         System.out.println("Thanks for choosing option 5");
     }
+
     private static void option6() {
         System.out.println("Thanks for choosing option 6");
     }
+
     private static void option7() {
         System.out.println("Thanks for choosing option 7");
     }
+
     private static void option8() {
         System.out.println("Thanks for choosing option 8");
     }
+
     private static void option9() {
         System.out.println("Thanks for choosing option 9");
     }
+
     private static void option10() {
         System.out.println("Thanks for choosing option 10");
     }
-    
+
 }
